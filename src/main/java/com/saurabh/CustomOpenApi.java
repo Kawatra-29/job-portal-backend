79109c8d1b1f45dd6f3817ec38fe2b2d95ba35a5
SAURABH <saurabhkawatra2001@gmail.com>
2026-03-24 19:33:05 +0530

    Fix Swagger HTTP to HTTPS issue

---
+++ src/main/java/com/saurabh/CustomOpenApi.java
@@ -0,0 +1,16 @@
+package com.saurabh;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class CustomOpenApi {
+	@Bean
+	OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.addServersItem(new Server().url("https://job-portal-backend-production-1bc7.up.railway.app"))
+				.addServersItem(new Server().url("http://localhost:8080"));
+	}
+}
